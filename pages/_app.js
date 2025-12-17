5bf3c365f98f7d63d89369eb22c7b2a7256daad2
Sampath Bommakanti <sampath.in@gmail.com>
2025-12-17 13:46:38 +0100

    make the website mobile responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        <meta name="theme-color" content="#000011" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
