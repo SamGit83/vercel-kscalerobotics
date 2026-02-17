32c50ac498ea73f2183f6da99d063d8df401eb5a
Sampath Bommakanti <sampath.in@gmail.com>
2026-02-17 14:12:35 +0100

    Updated for ahrefs analytics and google tag manager

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,17 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import Navigation from '../components/Navigation';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    var ahrefs_analytics_script = document.createElement('script');
+    ahrefs_analytics_script.async = true;
+    ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
+    ahrefs_analytics_script.setAttribute('data-key', 'qOXq0oEREO/Mq0i5gpxH2g');
+    document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
+  }, []);
+
   return (
     <>
       <Head>
